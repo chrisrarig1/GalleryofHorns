509b23d599cd507ca3ff221f518aeffec5c73460
Chris Rarig <cwrarig20@gmail.com>
2021-09-18 17:39:46 -0500

    added Horned Beast and updated JS code to render img, descritption, and names

---
+++ src/HornedBeast.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class HornedBeast extends React.Component{
+    render(){
+    return( 
+    <>
+        <h2>Title</h2>
+        <img src="https://via.placeholder.com/150" alt="placeholder" title="horns"></img>
+        <p>Decription of the Animal</p>
+    </>
+    )
+    }
+}
+
+export default HornedBeast;
