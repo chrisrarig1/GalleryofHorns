baf1dee551bf1afcbe85e9527f8c282bebb3e6f0
Chris Rarig <cwrarig20@gmail.com>
2021-09-21 22:47:38 -0500

    added horns array and display

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,39 @@
 import React from "react";
+import './HornedBeast.css';
 
 
 class HornedBeast extends React.Component{
+//have to add for state
+    constructor(props){
+        super(props);
+        //state must be an object
+        this.state = {
+            votes: 0,
+            Fav: false,
+        }
+    }
+    add = ()=>{
+        //how you set state
+        this.setState({votes: this.state.votes + 1})
+        console.log('voted');
+    }
+
+    // favorite = () => {
+    //     if(fav === true){
+
+    //     }
+    // }
+    //ternary statement is if/else rolled into one. evaluation ? true : false
+
     render(){
     return( 
-    <>
+    <div className='beasts'>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt="placeholder" title="horns"></img>
+        <img src={this.props.image} alt={this.props.keyword} horns={this.props.horns}></img>
         <p>{this.props.description}</p>
-    </>
+        <p>total votes: {this.state.votes}</p>
+        <button onClick={this.add}>Vote</button>
+    </div>
     )
     }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,22 @@
 import React from "react";
-import HornedBeast from "./HornedBeast";
+import HornedBeast from "./HornedBeast.js";
+import data from './data.json'
 
 class Main extends React.Component{
+// set functions
+
+
     render(){
+        //set variables
+        let beasts =[];
+        data.forEach((pics, idx) =>{
+            beasts.push(
+                <HornedBeast title={pics.title} image={pics.image_url} alt={pics.alt} description={pics.description} />
+            )
+        })
         return(
             <>
-                <HornedBeast title="A Beast" imageUrl="https://via.placeholder.com/150" description="beast" />
-                <HornedBeast title="Another" imageUrl="https://via.placeholder.com/150" description="bunch"  />
+                {beasts}
             </>
         )
     }
